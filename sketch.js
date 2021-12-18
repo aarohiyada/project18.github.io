74358ddc3e0607d51041c7ff0441c6bcfa679f6b
aarohiyada <93830155+aarohiyada@users.noreply.github.com>
2021-12-18 20:47:04 +0530

    project18
    project18

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var std;
+function setup() {
+  createCanvas(400,400);
+std = new Student("aarohi",13,8);
+std.show();
+}
+
+function draw() 
+{
+  background(30);
+
+}
---
+++ student.js
@@ -0,0 +1,17 @@
+
+class Student
+{
+   constructor(name,age,cls){
+       this.name = name;
+       this.age = age;
+       this.cls = cls;
+   }
+   show(){
+       console.log(this.name);
+       console.log(this.age);
+       console.log(this.cls);
+   }
+}
+
+
+
